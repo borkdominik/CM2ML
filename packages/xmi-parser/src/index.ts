0c47d1c8377b70f821cb1c10690201ad4de2fe00
Jan Müller <janmueller3698@gmail.com>
2023-09-29 10:55:27 +0200

    refactor(xmi-parser): extract mappers for readability

--- packages/xmi-parser/src/index.ts
+++ packages/xmi-parser/src/index.ts
@@ -15,68 +15,12 @@ export function parse(xmi: string) {
 function getModel(document: Document): XmiModel {
   const elementMap = new Map<string, XmiElement>()
 
-  function mapDocument(document: Document) {
-    return Stream.from(document.childNodes)
-      .map((child) => (isElement(child) ? mapElement(child) : null))
-      .filterNonNull()
-  }
-
-  function mapAttributes(
-    attributes: Record<string, string>
-  ): Record<XmiAttributeName, XmiAttribute> {
-    return Stream.fromObject(attributes)
-      .map(mapAttribute)
-      .toRecord(
-        ({ name }) => name,
-        (attribute) => attribute
-      )
-  }
-
-  function mapAttribute([name, value]: [string, string]): XmiAttribute {
-    const xmiValue = mapValue(value)
-    if (!name.includes(':')) {
-      return { name, value: xmiValue }
-    }
-    const [namespace, ...rest] = name.split(':')
-    return { name: rest.join(':'), value: xmiValue, namespace }
-  }
-
-  function mapValue(value: string): XmiValue {
-    if (!value.includes(':')) {
-      return { literal: value }
-    }
-    const [namespace, ...rest] = value.split(':')
-    return { literal: rest.join(':'), namespace }
-  }
-
-  function mapElement(element: Element): XmiElement | null {
-    const attributes = mapAttributes(element.attribs)
-    const children = Stream.from(element.childNodes)
-      .map((child) => (isElement(child) ? mapElement(child) : null))
-      .filterNonNull()
-      .toArray()
-    const xmiElement = new XmiElement(element.tagName, attributes, children)
-    children.forEach((child) => (child.parent = xmiElement))
-    registerElement(xmiElement)
-    return xmiElement
-  }
-
   function registerElement(element: XmiElement) {
     const id = element.getAttribute('id')
     if (id === undefined) {
       return
     }
     elementMap.set(id.value.literal, element)
-  }
-
-  function isElement(node: Node): node is Element {
-    return node.type === 'tag' || node instanceof Element
-  }
-
-  function collectAllElements(element: XmiElement): Stream<XmiElement> {
-    return Stream.from(element.children)
-      .flatMap(collectAllElements)
-      .append(element)
   }
 
   function getRelationship(element: XmiElement) {
@@ -95,7 +39,10 @@ function getModel(document: Document): XmiModel {
     return new XmiRelationship(element.tag, source, target)
   }
 
-  const elements = mapDocument(document).flatMap(collectAllElements).toArray()
+  const elements = mapDocument(document)
+    .flatMap(collectAllElements)
+    .forEach(registerElement)
+    .toArray()
 
   const relationships = Stream.from(elements)
     .map(getRelationship)
@@ -105,8 +52,58 @@ function getModel(document: Document): XmiModel {
   return new XmiModel(elements, relationships)
 }
 
-function getId(element: XmiElement): string | undefined {
-  return element.getAttribute('id')?.value.literal
+function mapDocument(document: Document) {
+  return Stream.from(document.childNodes)
+    .map((child) => (isElement(child) ? mapElement(child) : null))
+    .filterNonNull()
+}
+function mapElement(element: Element): XmiElement | null {
+  const attributes = mapAttributes(element.attribs)
+  const children = Stream.from(element.childNodes)
+    .map((child) => (isElement(child) ? mapElement(child) : null))
+    .filterNonNull()
+    .toArray()
+  const xmiElement = new XmiElement(element.tagName, attributes, children)
+  children.forEach((child) => (child.parent = xmiElement))
+  return xmiElement
+}
+
+function mapAttributes(
+  attributes: Record<string, string>
+): Record<XmiAttributeName, XmiAttribute> {
+  return Stream.fromObject(attributes)
+    .map(mapAttribute)
+    .toRecord(
+      ({ name }) => name,
+      (attribute) => attribute
+    )
+}
+
+function mapAttribute([name, value]: [string, string]): XmiAttribute {
+  const xmiValue = mapValue(value)
+  if (!name.includes(':')) {
+    return { name, value: xmiValue }
+  }
+  const [namespace, ...rest] = name.split(':')
+  return { name: rest.join(':'), value: xmiValue, namespace }
+}
+
+function mapValue(value: string): XmiValue {
+  if (!value.includes(':')) {
+    return { literal: value }
+  }
+  const [namespace, ...rest] = value.split(':')
+  return { literal: rest.join(':'), namespace }
+}
+
+function isElement(node: Node): node is Element {
+  return node.type === 'tag' || node instanceof Element
+}
+
+function collectAllElements(element: XmiElement): Stream<XmiElement> {
+  return Stream.from(element.children)
+    .flatMap(collectAllElements)
+    .append(element)
 }
 
 function getNearestIdentifiableElement(element: XmiElement) {
@@ -118,4 +115,8 @@ function getNearestIdentifiableElement(element: XmiElement) {
     target = target.parent
   }
   return target
+}
+
+function getId(element: XmiElement): string | undefined {
+  return element.getAttribute('id')?.value.literal
 }
