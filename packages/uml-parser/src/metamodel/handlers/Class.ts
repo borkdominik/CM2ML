8547abbe73de26cf8d08ef2e7794447dd5705b3d
Jan Müller <janmueller3698@gmail.com>
2023-12-19 14:31:40 +0100

    feat(uml): stub interface handler

--- packages/uml-parser/src/metamodel/handlers/Class.ts
+++ packages/uml-parser/src/metamodel/handlers/Class.ts
@@ -3,41 +3,41 @@ import type { GraphNode } from '@cm2ml/ir'
 import { Uml } from '../../uml'
 import { Class, Property } from '../metamodel'
 
-export const ClassHandler = Class.createHandler((clazz) => {
-  addEdge_extension(clazz)
-  addEdge_superClass(clazz)
-  clazz.children.forEach((child) => {
-    addEdge_nestedClassifier(clazz, child)
-    addEdge_ownedAttribute(clazz, child)
-    addEdge_ownedReception(clazz, child)
+export const ClassHandler = Class.createHandler((class_) => {
+  addEdge_extension(class_)
+  addEdge_superClass(class_)
+  class_.children.forEach((child) => {
+    addEdge_nestedClassifier(class_, child)
+    addEdge_ownedAttribute(class_, child)
+    addEdge_ownedReception(class_, child)
   })
 })
 
-function addEdge_extension(_clazz: GraphNode) {
+function addEdge_extension(_class_: GraphNode) {
   // TODO
   // /extension : Extension [0..*]{} (opposite Extension::metaclass)
   // This property is used when the Class is acting as a metaclass. It references the Extensions that specify additional properties of the metaclass. The property is derived from the Extensions whose memberEnds are typed by the Class.
 }
 
-function addEdge_nestedClassifier(clazz: GraphNode, child: GraphNode) {
+function addEdge_nestedClassifier(class_: GraphNode, child: GraphNode) {
   if (Class.isAssignable(child)) {
-    clazz.model.addEdge('nestedClassifier', clazz, child)
+    class_.model.addEdge('nestedClassifier', class_, child)
   }
 }
 
-function addEdge_ownedAttribute(clazz: GraphNode, child: GraphNode) {
+function addEdge_ownedAttribute(class_: GraphNode, child: GraphNode) {
   if (Property.isAssignable(child)) {
-    clazz.model.addEdge(Uml.Tags.ownedAttribute, clazz, child)
+    class_.model.addEdge(Uml.Tags.ownedAttribute, class_, child)
   }
 }
 
-function addEdge_ownedReception(_clazz: GraphNode, _child: GraphNode) {
+function addEdge_ownedReception(_class_: GraphNode, _child: GraphNode) {
   // TODO
   // ♦ ownedReception : Reception [0..*]{subsets Classifier::feature, subsets Namespace::ownedMember} (opposite A_ownedReception_class::class)
   // The Receptions owned by the Class.
 }
 
-function addEdge_superClass(_clazz: GraphNode) {
+function addEdge_superClass(_class_: GraphNode) {
   // TODO
   // /superClass : Class [0..*]{redefines Classifier::general} (opposite A_superClass_class::class)
   // The superclasses of a Class, derived from its Generalizations.
