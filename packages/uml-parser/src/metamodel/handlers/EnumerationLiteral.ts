6d90247fedbc02a0c5e506adc3428936be9ddc17
Jan Müller <janmueller3698@gmail.com>
2023-12-19 14:15:10 +0100

    feat(uml): stub enumeration literal handler

--- packages/uml-parser/src/metamodel/handlers/EnumerationLiteral.ts
+++ packages/uml-parser/src/metamodel/handlers/EnumerationLiteral.ts
@@ -1,4 +1,24 @@
+import type { GraphNode } from '@cm2ml/ir'
+
 import { EnumerationLiteral } from '../metamodel'
 
-// TODO
-export const EnumerationLiteralHandler = EnumerationLiteral.createHandler()
+export const EnumerationLiteralHandler = EnumerationLiteral.createHandler(
+  (enumerationLiteral) => {
+    addEdge_classifier(enumerationLiteral)
+    addEdge_enumeration(enumerationLiteral)
+  },
+)
+
+function addEdge_classifier(_enumerationLiteral: GraphNode) {
+  // TODO
+  // /classifier : Enumeration [1..1]{redefines InstanceSpecification::classifier} (opposite A_classifier_enumerationLiteral::enumerationLiteral )
+  // The classifier of this EnumerationLiteral derived to be equal to its Enumeration.
+}
+
+function addEdge_enumeration(enumerationLiteral: GraphNode) {
+  const parent = enumerationLiteral.parent
+  if (!parent) {
+    throw new Error('Missing parent for EnumerationLiteral')
+  }
+  enumerationLiteral.model.addEdge('enumeration', enumerationLiteral, parent)
+}
