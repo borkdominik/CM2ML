2c5aca2f6e82f7a16eaa90a7e0691c4b9d3b2c29
Jan Müller <janmueller3698@gmail.com>
2023-12-19 13:54:15 +0100

    feat(uml): implement element handler

--- packages/uml-parser/src/metamodel/handlers/Element.ts
+++ packages/uml-parser/src/metamodel/handlers/Element.ts
@@ -1,3 +1,30 @@
-import { Element } from '../metamodel'
+import type { GraphNode } from '@cm2ml/ir'
 
-export const ElementHandler = Element.createHandler()
+import { Comment, Element } from '../metamodel'
+
+export const ElementHandler = Element.createHandler((element) => {
+  addEdge_owner(element)
+  element.children.forEach((child) => {
+    addEdge_ownedElement(element, child)
+    addEdge_ownedComment(element, child)
+  })
+})
+
+function addEdge_owner(element: GraphNode) {
+  const parent = element.parent
+  if (parent && Element.isAssignable(parent)) {
+    element.model.addEdge('owner', element, parent)
+  }
+}
+
+function addEdge_ownedElement(element: GraphNode, child: GraphNode) {
+  if (Element.isAssignable(child)) {
+    element.model.addEdge('ownedElement', element, child)
+  }
+}
+
+function addEdge_ownedComment(element: GraphNode, child: GraphNode) {
+  if (Comment.isAssignable(child)) {
+    element.model.addEdge('ownedComment', element, child)
+  }
+}
