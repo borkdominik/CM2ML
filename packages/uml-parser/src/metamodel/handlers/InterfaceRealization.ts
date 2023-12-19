fdb4d8fabb4575d194cb3716f05cf9636bcf5a86
Jan Müller <janmueller3698@gmail.com>
2023-12-19 14:35:02 +0100

    feat(uml): implement interface realization handler

--- packages/uml-parser/src/metamodel/handlers/InterfaceRealization.ts
+++ packages/uml-parser/src/metamodel/handlers/InterfaceRealization.ts
@@ -1,38 +1,42 @@
+import type { GraphNode } from '@cm2ml/ir'
+
 import { InterfaceRealization } from '../metamodel'
 
 export const InterfaceRealizationHandler = InterfaceRealization.createHandler(
-  (node) => {
-    // Contract
-    const contract = node.getAttribute('contract')?.value.literal
-    if (contract === undefined) {
-      throw new Error('InterfaceRealization must have a contract')
-    }
-    const resolvedContract = node.model.getNodeById(contract)
-    if (!resolvedContract) {
-      throw new Error('InterfaceRealization must have a resolved contract')
-    }
-    node.model.addEdge('contract', node, resolvedContract)
-
-    // ImplementingClassifier
-    const implementingClassifierId = node.getAttribute('client')?.value.literal
-    if (implementingClassifierId === undefined) {
-      throw new Error('InterfaceRealization must have a client')
-    }
-    const resolvedImplementingClassifier = node.model.getNodeById(
-      implementingClassifierId,
-    )
-    if (!resolvedImplementingClassifier) {
-      throw new Error('InterfaceRealization must have a resolved client')
-    }
-    node.model.addEdge(
-      'interfaceRealization',
-      resolvedImplementingClassifier,
-      node,
-    )
-    node.model.addEdge(
-      'implementingClassifier',
-      node,
-      resolvedImplementingClassifier,
-    )
+  (interfaceRealization) => {
+    addEdge_contract(interfaceRealization)
+    addEdge_implementingClassifier(interfaceRealization)
   },
 )
+
+function addEdge_contract(interfaceRealization: GraphNode) {
+  const contractId =
+    interfaceRealization.getAttribute('contract')?.value.literal
+  if (contractId === undefined) {
+    throw new Error('InterfaceRealization must have a contract')
+  }
+  const contract = interfaceRealization.model.getNodeById(contractId)
+  if (!contract) {
+    throw new Error('InterfaceRealization must have a resolved contract')
+  }
+  interfaceRealization.model.addEdge('contract', interfaceRealization, contract)
+}
+
+function addEdge_implementingClassifier(interfaceRealization: GraphNode) {
+  const implementingClassifierId =
+    interfaceRealization.getAttribute('client')?.value.literal
+  if (implementingClassifierId === undefined) {
+    throw new Error('InterfaceRealization must have a client')
+  }
+  const implementingClassifier = interfaceRealization.model.getNodeById(
+    implementingClassifierId,
+  )
+  if (!implementingClassifier) {
+    throw new Error('InterfaceRealization must have a resolved client')
+  }
+  interfaceRealization.model.addEdge(
+    'implementingClassifier',
+    interfaceRealization,
+    implementingClassifier,
+  )
+}
