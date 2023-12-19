181631f42fdec50dd75b317fdd746d97da161a6d
Jan Müller <janmueller3698@gmail.com>
2023-12-19 13:58:27 +0100

    feat(uml): implement data type handler

--- packages/uml-parser/src/metamodel/handlers/DataType.ts
+++ packages/uml-parser/src/metamodel/handlers/DataType.ts
@@ -1,3 +1,23 @@
-import { DataType } from '../metamodel'
+import type { GraphNode } from '@cm2ml/ir'
 
-export const DataTypeHandler = DataType.createHandler()
+import { Uml } from '../../uml'
+import { DataType, Operation, Property } from '../metamodel'
+
+export const DataTypeHandler = DataType.createHandler((dataType) => {
+  dataType.children.forEach((child) => {
+    addEdge_ownedAttribute(dataType, child)
+    addEdge_ownedOperation(dataType, child)
+  })
+})
+
+function addEdge_ownedAttribute(dataType: GraphNode, child: GraphNode) {
+  if (Property.isAssignable(child)) {
+    dataType.model.addEdge(Uml.Tags.ownedAttribute, dataType, child)
+  }
+}
+
+function addEdge_ownedOperation(dataType: GraphNode, child: GraphNode) {
+  if (Operation.isAssignable(child)) {
+    dataType.model.addEdge(Uml.Tags.ownedOperation, dataType, child)
+  }
+}
