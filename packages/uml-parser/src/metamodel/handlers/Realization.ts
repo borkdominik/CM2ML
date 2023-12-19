be1230b19b9a55aab53f04fae4e0a7860018eff4
Jan Müller <janmueller3698@gmail.com>
2023-12-19 15:10:29 +0100

    feat(uml): stub realization

--- packages/uml-parser/src/metamodel/handlers/Realization.ts
+++ packages/uml-parser/src/metamodel/handlers/Realization.ts
@@ -1,3 +1,13 @@
+import type { GraphNode } from '@cm2ml/ir'
+
 import { Realization } from '../metamodel'
 
-export const RealizationHandler = Realization.createHandler()
+export const RealizationHandler = Realization.createHandler((realization) => {
+  addEdge_relatedElement(realization)
+})
+
+function addEdge_relatedElement(_realization: GraphNode) {
+  // TODO
+  // /relatedElement : Element [1..*]{union} (opposite A_relatedElement_relationship::relationship)
+  // Specifies the elements related by the Relationship.
+}
