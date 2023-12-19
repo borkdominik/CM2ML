b6288d16bece9e98ffba1ef10b32b86f107188c7
Jan Müller <janmueller3698@gmail.com>
2023-12-19 13:54:46 +0100

    feat(uml): stub abstraction handler

--- packages/uml-parser/src/metamodel/handlers/Abstraction.ts
+++ packages/uml-parser/src/metamodel/handlers/Abstraction.ts
@@ -1,3 +1,12 @@
+import type { GraphNode } from '@cm2ml/ir'
+
 import { Abstraction } from '../metamodel'
 
-export const AbstractionHandler = Abstraction.createHandler()
+export const AbstractionHandler = Abstraction.createHandler((abstraction) => {
+  addEdge_mapping(abstraction)
+})
+
+function addEdge_mapping(_abstraction: GraphNode) {
+  // TODO
+  // mapping: OpaqueExpression[0..1]{subsets Element:: ownedElement } (opposite A_mapping_abstraction::abstraction)
+}
