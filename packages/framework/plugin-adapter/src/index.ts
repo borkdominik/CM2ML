31182867aec7be5c1687139ddb19a5ef859f9e91
Jan Müller <janmueller3698@gmail.com>
2024-03-28 09:04:41 +0100

    feat(plugin-adapter): implement support for batched plugins

--- packages/framework/plugin-adapter/src/index.ts
+++ packages/framework/plugin-adapter/src/index.ts
@@ -1,30 +1,51 @@
 import type { ParameterMetadata, Plugin } from '@cm2ml/plugin'
 import { Stream } from '@yeger/streams'
 
-export abstract class PluginAdapter<In> {
-  protected plugins = new Map<string, Plugin<In, unknown, any>>()
+export interface PluginAdapterConfiguration {
+  batched: boolean
+}
+
+export type SupportedPlugin<In> = Plugin<In, unknown, ParameterMetadata> | Plugin<In[], unknown[], ParameterMetadata>
+
+export abstract class PluginAdapter<In, Configuration extends PluginAdapterConfiguration = PluginAdapterConfiguration> {
+  protected plugins = new Map<string, Plugin<In, unknown, any> | Plugin<In[], unknown[], any>>()
 
   private started = false
 
-  public applyAll(plugins: Plugin<In, unknown, ParameterMetadata>[]) {
-    Stream.from(plugins).forEach((plugin) => this.apply(plugin))
+  /**
+   *
+   * Use {@link apply} for per-plugin configurations.
+   */
+  public applyAll(plugins: SupportedPlugin<In>[], configuration: Configuration) {
+    Stream.from(plugins).forEach((plugin) => this.apply(plugin, configuration))
     return this
   }
 
   public apply(
-    plugin: Plugin<In, unknown, ParameterMetadata>,
-  ): PluginAdapter<In> {
+    plugin: SupportedPlugin<In>,
+    configuration: Configuration,
+  ): PluginAdapter<In, Configuration> {
     this.requireNotStarted()
     if (this.plugins.has(plugin.name)) {
       throw new Error(`Plugin ${plugin.name} already applied.`)
     }
     this.plugins.set(plugin.name, plugin)
-    this.onApply(plugin)
+    if (configuration.batched) {
+      this.onApplyBatched(plugin as Plugin<In[], unknown[], ParameterMetadata>, configuration)
+      return this
+    }
+    this.onApply(plugin as Plugin<In, unknown, ParameterMetadata>, configuration)
     return this
   }
 
   protected abstract onApply<Out, Parameters extends ParameterMetadata>(
     plugin: Plugin<In, Out, Parameters>,
+    configuration: Configuration,
+  ): void
+
+  protected abstract onApplyBatched<Out, Parameters extends ParameterMetadata>(
+    plugin: Plugin<In[], Out[], Parameters>,
+    configuration: Configuration,
   ): void
 
   public start() {
