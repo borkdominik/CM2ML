dd2435f6f614d913d1c55e764866884106559225
Jan Müller <janmueller3698@gmail.com>
2024-03-28 09:05:14 +0100

    feat(cli): implement support for batched plugins

--- packages/framework/cli-adapter/src/index.ts
+++ packages/framework/cli-adapter/src/index.ts
@@ -1,61 +1,40 @@
 import fs from 'node:fs'
+import path from 'node:path'
 import process from 'node:process'
 
 import type { Parameter, ParameterMetadata, Plugin } from '@cm2ml/plugin'
 import { getTypeConstructor } from '@cm2ml/plugin'
+import type { PluginAdapterConfiguration } from '@cm2ml/plugin-adapter'
 import { PluginAdapter } from '@cm2ml/plugin-adapter'
 import { getMessage } from '@cm2ml/utils'
 import { Stream } from '@yeger/streams'
+import type { Command } from 'cac'
 import { cac } from 'cac'
 
-class CLI extends PluginAdapter<string> {
-  private readonly cli = cac('cm2ml').option(
-    '--out <file>',
-    'Path to output file',
-  )
+class CLI extends PluginAdapter<string, PluginAdapterConfiguration> {
+  private readonly cli = cac('cm2ml')
+    .option(
+      '--out <file/directory>',
+      'Path to output file (or directory for batched mode)',
+      { type: [(input: unknown) => input === undefined ? undefined : String(input)] },
+    )
+    .option('--pretty', 'Pretty print JSON output', { default: false, type: [Boolean] })
 
   protected onApply<Out, Parameters extends ParameterMetadata>(
     plugin: Plugin<string, Out, Parameters>,
   ) {
     const command = this.cli.command(`${plugin.name} <inputFile>`)
-    Stream.fromObject(plugin.parameters).forEach(([name, parameter]) => {
-      if (parameter.type !== 'boolean') {
-        command.option(
-          `--${createOptionName(name)} <${name}>`,
-          createOptionDescription(parameter),
-          {
-            default: parameter.defaultValue,
-            type: [getTypeConstructor(parameter.type)],
-          },
-        )
-        return
-      }
-      if (parameter.defaultValue !== true) {
-        command.option(
-          `--${createOptionName(name)}`,
-          createOptionDescription(parameter),
-          {
-            default: parameter.defaultValue,
-            type: [getTypeConstructor(parameter.type)],
-          },
-        )
-      }
-      if (parameter.defaultValue !== false) {
-        const alteredDescription =
-          parameter.description.substring(0, 1).toLowerCase() +
-          parameter.description.substring(1)
-        command.option(
-          `--no-${createOptionName(name)}`,
-          `Do not ${alteredDescription}`,
-          {
-            default: parameter.defaultValue,
-            type: [getTypeConstructor(parameter.type)],
-          },
-        )
-      }
-    })
+    registerCommandOptions(command, plugin.parameters)
     command.action((inputFile: string, options: Record<string, unknown>) =>
       pluginActionHandler(plugin, inputFile, options),
+    )
+  }
+
+  protected onApplyBatched<Out, Parameters extends ParameterMetadata>(plugin: Plugin<string[], Out[], Parameters>) {
+    const command = this.cli.command(`${plugin.name} <inputDir>`)
+    registerCommandOptions(command, plugin.parameters)
+    command.action((inputFile: string, options: Record<string, unknown>) =>
+      batchedPluginActionHandler(plugin, inputFile, options),
     )
   }
 
@@ -69,6 +48,48 @@ class CLI extends PluginAdapter<string> {
       process.exit(1)
     }
   }
+}
+
+function registerCommandOptions<Parameters extends ParameterMetadata>(
+  command: Command,
+  parameters: Parameters,
+) {
+  Stream.fromObject(parameters).forEach(([name, parameter]) => {
+    if (parameter.type !== 'boolean') {
+      command.option(
+          `--${createOptionName(name)} <${name}>`,
+          createOptionDescription(parameter),
+          {
+            default: parameter.defaultValue,
+            type: [getTypeConstructor(parameter.type)],
+          },
+      )
+      return
+    }
+    if (parameter.defaultValue !== true) {
+      command.option(
+          `--${createOptionName(name)}`,
+          createOptionDescription(parameter),
+          {
+            default: parameter.defaultValue,
+            type: [getTypeConstructor(parameter.type)],
+          },
+      )
+    }
+    if (parameter.defaultValue !== false) {
+      const alteredDescription =
+          parameter.description.substring(0, 1).toLowerCase() +
+          parameter.description.substring(1)
+      command.option(
+          `--no-${createOptionName(name)}`,
+          `Do not ${alteredDescription}`,
+          {
+            default: parameter.defaultValue,
+            type: [getTypeConstructor(parameter.type)],
+          },
+      )
+    }
+  })
 }
 
 function createOptionName(parameterName: string) {
@@ -94,24 +115,10 @@ function pluginActionHandler<Out, Parameters extends ParameterMetadata>(
   inputFile: string,
   options: Record<string, unknown>,
 ) {
-  const normalizedOptions: Record<string, unknown> & { out?: string } =
-    Stream.fromObject(options)
-      .map(([name, parameter]) => {
-        if (Array.isArray(parameter)) {
-          return [name, parameter[0]]
-        } else {
-          return [name, parameter]
-        }
-      })
-      .toRecord(
-        ([name]) => name,
-        ([_name, value]) => value,
-      )
-
+  const normalizedOptions = normalizeOptions(options)
   const input = fs.readFileSync(inputFile, 'utf8')
   const result = plugin.validateAndInvoke(input, normalizedOptions)
-  const resultText =
-    typeof result === 'string' ? result : JSON.stringify(result)
+  const resultText = getResultAsText(result, normalizedOptions.pretty)
 
   const outFile = normalizedOptions.out
   if (!outFile) {
@@ -120,7 +127,60 @@ function pluginActionHandler<Out, Parameters extends ParameterMetadata>(
     return
   }
 
+  fs.mkdirSync(path.dirname(outFile), { recursive: true })
   fs.writeFileSync(outFile, resultText)
+}
+
+function batchedPluginActionHandler<Out, Parameters extends ParameterMetadata>(
+  plugin: Plugin<string[], Out[], Parameters>,
+  inputDir: string,
+  options: Record<string, unknown>,
+) {
+  const normalizedOptions = normalizeOptions(options)
+  const inputFiles = fs.readdirSync(inputDir, { encoding: 'utf8', withFileTypes: true }).filter((dirent) => dirent.isFile()).map((dirent) => dirent.name)
+  const input = inputFiles.map((inputFile) => fs.readFileSync(`${inputDir}/${inputFile}`, 'utf8'))
+  const results = plugin.validateAndInvoke(input, normalizedOptions)
+
+  const outDir = normalizedOptions.out
+  if (!outDir) {
+    results.forEach((result, index) => {
+      const resultText = getResultAsText(result, normalizedOptions.pretty)
+      // eslint-disable-next-line no-console
+      console.log(`\n${inputFiles[index]}:\n${resultText}\n`)
+    })
+
+    return
+  }
+
+  fs.mkdirSync(outDir, { recursive: true })
+  results.forEach((result, index) => {
+    const inputFileName = inputFiles[index]
+    if (!inputFileName) {
+      throw new Error('Internal error. Input file is missing')
+    }
+    const outFile = `${outDir}/${inputFileName}.${plugin.name}.json`
+    const resultText = getResultAsText(result, normalizedOptions.pretty)
+    fs.writeFileSync(outFile, resultText)
+  })
+}
+
+function getResultAsText(result: unknown, pretty: boolean | undefined): string {
+  return typeof result === 'string' ? result : JSON.stringify(result, null, pretty ? 2 : undefined)
+}
+
+function normalizeOptions(options: Record<string, unknown>): Record<string, unknown> & { out?: string, pretty?: boolean } {
+  return Stream.fromObject(options)
+    .map(([name, parameter]) => {
+      if (Array.isArray(parameter)) {
+        return [name, parameter[0]]
+      } else {
+        return [name, parameter]
+      }
+    })
+    .toRecord(
+      ([name]) => name,
+      ([_name, value]) => value,
+    )
 }
 
 export function createCLI() {
--- packages/framework/cli/src/index.ts
+++ packages/framework/cli/src/index.ts
@@ -1,6 +1,9 @@
-import { plugins } from '@cm2ml/builtin'
+import { batchedPlugins, plugins } from '@cm2ml/builtin'
 import { createCLI } from '@cm2ml/cli-adapter'
 
 export function startPreConfiguredCli() {
-  createCLI().applyAll(plugins).start()
+  createCLI()
+    .applyAll(batchedPlugins, { batched: true })
+    .applyAll(plugins, { batched: false })
+    .start()
 }
