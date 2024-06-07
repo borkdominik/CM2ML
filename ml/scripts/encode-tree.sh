23b334c6f3534adadb6d4942e4d9174f3903c504
Jan Müller <janmueller3698@gmail.com>
2024-06-07 07:54:21 +0200

    feat(ml): add `encode:tree` task

---
+++ ml/scripts/encode-tree.sh
@@ -0,0 +1,15 @@
+train_size=200
+validation_size=50
+test_size=50
+
+train_start=1
+validation_start=$(($train_size + $train_start))
+test_start=$(($validation_start + $validation_size + 1))
+
+input=../models/uml/dataset
+
+parameters=("--strict" "--merge" "--continue-on-error" "--relationships-as-edges")
+
+time bun node_modules/@cm2ml/cli/bin/cm2ml.mjs batch-uml-tree "$input" --start "$train_start" --limit "$train_size" --out .input/tree_train.json "${parameters[@]}" && \
+time bun node_modules/@cm2ml/cli/bin/cm2ml.mjs batch-uml-tree "$input" --start "$validation_start" --limit "$validation_size" --out .input/tree_validation.json  "${parameters[@]}" && \
+time bun node_modules/@cm2ml/cli/bin/cm2ml.mjs batch-uml-tree "$input" --start "$test_start" --limit "$test_size" --out .input/tree_test.json "${parameters[@]}"
