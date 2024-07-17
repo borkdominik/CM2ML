b22b84d6222662da56bb80b55f1df599f488fedc
Jan Müller <janmueller3698@gmail.com>
2024-07-17 13:49:33 +0200

    feat(bop): update evaluation script

--- ml/scripts/encode-pattern.sh
+++ ml/scripts/encode-pattern.sh
@@ -1,12 +1,13 @@
 time bun node_modules/@cm2ml/cli/bin/cm2ml.mjs batch-uml-bag-of-paths ../models/uml/dataset \
   --out .output/patterns.json \
   --strict --pretty \
-  --continue-on-error --start 1 --limit 200 \
+  --continue-on-error --start 1 --limit 10000 \
   --only-containment-associations --relationships-as-edges \
-  --max-partitioning-iterations 9999 --max-partition-size 4 \
-  --mask-node-types \
+  --edge-blacklist "association" --edge-blacklist "owner" --edge-blacklist "ownedElement" --edge-blacklist "usage" \
+  --max-partitioning-iterations 9999 --max-partition-size 8 \
+  --mask-node-types false \
   --min-pattern-length 3 --max-pattern-length 1000 \
-  --max-patterns-per-partition 10 \
+  --max-patterns-per-partition 50 \
   --min-model-frequency 3 --min-absolute-frequency 5 \
-  --max-patterns 10 --pattern-order absolute-frequency
+  --max-patterns 20 --pattern-order model-frequency
 
