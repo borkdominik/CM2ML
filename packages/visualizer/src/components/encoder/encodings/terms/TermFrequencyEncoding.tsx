94671569326f3aae12f10eeea7a8fe023db682bb
plglaser <plg741@gmail.com>
2024-08-10 01:01:16 +0200

    fix(tf-encoder): fix type mismatch

--- packages/visualizer/src/components/encoder/encodings/terms/TermFrequencyEncoding.tsx
+++ packages/visualizer/src/components/encoder/encodings/terms/TermFrequencyEncoding.tsx
@@ -70,7 +70,7 @@ export function TermFrequencyEncoding({ model, parameters }: Props) {
                 </TableCell>
                 {modelIds.map((modelId: string) => (
                   <TableCell key={modelId} className="border px-4 py-2 text-center">
-                    {termDocumentMatrix[modelId][index]}
+                    {termDocumentMatrix[modelId]![index]}
                   </TableCell>
                 ))}
               </TableRow>
