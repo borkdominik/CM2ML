40c6da5e115d530cc27336ad07f0f901f20040c0
Jan Müller <janmueller3698@gmail.com>
2024-09-07 22:13:37 +0200

    fix(visualizer): display errors monospaced

--- packages/visualizer/src/components/Error.tsx
+++ packages/visualizer/src/components/Error.tsx
@@ -3,7 +3,7 @@ import { getMessage } from '@cm2ml/utils'
 export function Error({ error }: { error: unknown }) {
   const message = getMessage(error)
   return (
-    <div className="border-input bg-destructive text-destructive-foreground flex items-center whitespace-pre text-balance rounded-md border px-3 py-1 text-xs">
+    <div className="flex items-center whitespace-pre text-balance rounded-md border border-input bg-destructive px-3 py-1 font-mono text-xs text-destructive-foreground">
       {message}
     </div>
   )
