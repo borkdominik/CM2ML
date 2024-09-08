b2f9f0efdba0aefe50d14fd4ed42de4a6af8fe3c
Jan Müller <janmueller3698@gmail.com>
2024-09-08 08:20:33 +0200

    perf(bop): improve cache

--- packages/encoder/bag-of-paths-encoder/src/paths.ts
+++ packages/encoder/bag-of-paths-encoder/src/paths.ts
@@ -32,7 +32,7 @@ export function collectPaths(model: GraphModel, parameters: PathParameters) {
   const nodes = Stream.from(model.nodes)
   const indexMap = new Map(nodes.map((node, i) => [node, i]))
   const getNodeIndex = (node: GraphNode) => indexMap.get(node)!
-  const cache: Map<GraphNode, Record<number, string | null>> = new Map()
+  const cache: Cache = []
   const paths = nodes
     .flatMap((node) => Path.from(node, parameters))
     .filter((path) => path.steps.length >= parameters.minPathLength)
@@ -148,23 +148,26 @@ function reduceWeights(weights: number[], type: PathWeight) {
   throw new Error(`Unsupported weight reduction: ${type}`)
 }
 
-function encodePath(steps: number[], nodes: GraphNode[], templates: Template<GraphNode>[], cache: Map<GraphNode, Record<number, string | null>>) {
+type Cache = Map<GraphNode, string | null>[]
+
+function encodePath(steps: number[], nodes: GraphNode[], templates: Template<GraphNode>[], cache: Cache) {
   const parts: (string | null)[] = []
   steps.forEach((step, stepIndex) => {
     const node = nodes[step]
     if (!node) {
       throw new Error(`Node index out-of-bounds. This is an internal error.`)
     }
-    if (!cache.has(node)) {
-      cache.set(node, {})
+    if (cache.length <= stepIndex) {
+      cache[stepIndex] = new Map<GraphNode, string | null>()
     }
-    const nodeCache = cache.get(node)!
-    if (nodeCache[stepIndex] !== undefined) {
-      parts.push(nodeCache[stepIndex])
+    const nodeCache = cache[stepIndex]!
+    const cachedValue = nodeCache.get(node)
+    if (cachedValue !== undefined) {
+      parts.push(cachedValue)
       return
     }
     const mappedNode = Stream.from(templates).map((template) => template(node, stepIndex)).find((part) => part !== undefined) ?? null
-    nodeCache[stepIndex] = mappedNode
+    nodeCache.set(node, mappedNode)
     parts.push(mappedNode)
   })
   // TODO/Jan: Also encode the edges
