760de90b3d0e16e2cf293f42084898dcc442b9c4
Jan Müller <janmueller3698@gmail.com>
2024-09-13 14:50:34 +0200

    feat(visualizer): use codemirror for raw json visualization

--- packages/visualizer/src/components/encoder/EncodingFallback.tsx
+++ packages/visualizer/src/components/encoder/EncodingFallback.tsx
@@ -1,8 +1,11 @@
 import type { Encoder } from '@cm2ml/builtin'
 import type { GraphModel } from '@cm2ml/ir'
+import { json } from '@codemirror/lang-json'
+import CodeMirror from '@uiw/react-codemirror'
 import { useMemo } from 'react'
 
 import { prettifyEncoderName } from '../../lib/pluginNames'
+import { useTheme } from '../../lib/useTheme'
 import type { ParameterValues } from '../Parameters'
 import { Hint } from '../ui/hint'
 import { Separator } from '../ui/separator'
@@ -18,16 +21,24 @@ export interface Props {
 export function EncodingFallback({ encoder, model, parameters }: Props) {
   const { encoding, error } = useEncoder(encoder, model, parameters)
   const formattedOutput = useMemo(() => JSON.stringify(encoding, null, 2), [encoding])
+  const theme = useTheme.use.theme()
   if (error !== undefined) {
     return <Hint error={error} />
   }
   return (
     <div className="flex h-full flex-col">
-      <Hint error={`No visualization for ${prettifyEncoderName(encoder.name)} available.`} />
+      <div>
+        <Hint error={`No visualization for ${prettifyEncoderName(encoder.name)} available.`} />
+      </div>
       <Separator />
-      <pre className="grow overflow-auto p-2 text-xs">
-        {formattedOutput}
-      </pre>
+      <div className="grow overflow-auto">
+        <CodeMirror
+          value={formattedOutput}
+          theme={theme}
+          readOnly
+          extensions={[json()]}
+        />
+      </div>
     </div>
   )
 }
