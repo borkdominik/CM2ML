6b282672879922c85c11b82cacd95bc5920ab763
Jan Müller <janmueller3698@gmail.com>
2024-10-14 15:18:34 +0200

    chore(gnn): change param order

--- ml/scripts/encode-graph.sh
+++ ml/scripts/encode-graph.sh
@@ -8,7 +8,7 @@ test_start=$(($validation_start + $validation_size + 1))
 
 input=../models/uml/dataset
 
-parameters=("--strict" "--deduplicate" "--continue-on-error" "--relationships-as-edges" "true" "--raw-strings" "--only-encoded-features" "--edge-tag-as-attribute" "true" "--only-containment-associations" "false")
+parameters=("--strict" "--deduplicate" "--continue-on-error" "--raw-strings" "--only-encoded-features" "--relationships-as-edges" "true" "--only-containment-associations" "false" "--edge-tag-as-attribute" "true")
 
 time bun node_modules/@cm2ml/cli/bin/cm2ml.mjs batch-uml-raw-graph "$input" --start "$train_start" --limit "$train_size" --out .input/graph_train.json "${parameters[@]}" && \
 time bun node_modules/@cm2ml/cli/bin/cm2ml.mjs batch-uml-raw-graph "$input" --start "$validation_start" --limit "$validation_size" --out .input/graph_validation.json "${parameters[@]}" --node-features ".input/graph_train.json" --edge-features ".input/graph_train.json" --deduplication-data ".input/graph_train.json" && \
