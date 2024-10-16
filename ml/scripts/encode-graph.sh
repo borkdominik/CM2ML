a262c3b291f53ccc1dc221ebb3f5bf181c0ebbfa
Jan Müller <janmueller3698@gmail.com>
2024-10-16 10:31:20 +0200

    chore: use multiline parameters

--- ml/scripts/encode-graph.sh
+++ ml/scripts/encode-graph.sh
@@ -8,7 +8,13 @@ test_start=$(($validation_start + $validation_size + 1))
 
 input=../models/uml/dataset
 
-parameters=("--strict" "--deduplicate" "--continue-on-error" "--raw-strings" "--only-encoded-features" "--relationships-as-edges" "true" "--only-containment-associations" "false" "--edge-tag-as-attribute" "true")
+parameters=(
+  "--strict" "--deduplicate" "--continue-on-error"
+  "--raw-strings" "--only-encoded-features"
+  "--relationships-as-edges" "true"
+  "--only-containment-associations" "true"
+  "--edge-tag-as-attribute" "false"
+)
 
 time bun node_modules/@cm2ml/cli/bin/cm2ml.mjs batch-uml-raw-graph "$input" --start "$train_start" --limit "$train_size" --out .input/graph_train.json "${parameters[@]}" && \
 time bun node_modules/@cm2ml/cli/bin/cm2ml.mjs batch-uml-raw-graph "$input" --start "$validation_start" --limit "$validation_size" --out .input/graph_validation.json "${parameters[@]}" --node-features ".input/graph_train.json" --edge-features ".input/graph_train.json" --deduplication-data ".input/graph_train.json" && \
