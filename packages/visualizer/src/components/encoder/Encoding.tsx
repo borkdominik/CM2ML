3d6cc4c7d9e89cd6fe48802b71f4238b63e03e5a
plglaser <plg741@gmail.com>
2025-06-04 13:15:27 +0200

    feat(visualizer): add new encodings to visualizer

--- packages/visualizer/src/components/encoder/Encoding.tsx
+++ packages/visualizer/src/components/encoder/Encoding.tsx
@@ -1,15 +1,18 @@
 import type { Encoder } from '@cm2ml/builtin'
-import { BagOfPathsEncoder, GraphEncoder, PatternMiner, TermFrequencyEncoder, TreeEncoder } from '@cm2ml/builtin'
+import { BagOfPathsEncoder, BagOfWordsEncoder, EmbeddingsEncoder, GraphEncoder, PatternMiner, TermFrequencyEncoder, TreeEncoder, TriplesEncoder } from '@cm2ml/builtin'
 import type { GraphModel } from '@cm2ml/ir'
 
 import type { ParameterValues } from '../Parameters'
 
 import { EncodingFallback } from './EncodingFallback'
 import { BagOfPathsEncoding } from './encodings/bag-of-paths/BagOfPathsEncoding'
+import { BagOfWordsEncoding } from './encodings/bag-of-words/BagOfWordsEncoding'
+import { EmbeddingsEncoding } from './encodings/embeddings/EmbeddingsEncoding'
 import { PatternEncoding } from './encodings/pattern/PatternEncoding'
 import { RawGraphEncoding } from './encodings/raw-graph/RawGraphEncoding'
 import { TermFrequencyEncoding } from './encodings/terms/TermFrequencyEncoding'
 import { TreeEncoding } from './encodings/tree/TreeEncoding'
+import { TriplesEncoding } from './encodings/triples/TriplesEncoding'
 
 export interface Props {
   encoder: Encoder
@@ -20,6 +23,9 @@ export interface Props {
 export function Encoding({ encoder, model, parameters }: Props) {
   if (encoder === BagOfPathsEncoder) {
     return <BagOfPathsEncoding model={model} parameters={parameters} />
+  }
+  if (encoder === BagOfWordsEncoder) {
+    return <BagOfWordsEncoding model={model} parameters={parameters} />
   }
   if (encoder === GraphEncoder) {
     return <RawGraphEncoding model={model} parameters={parameters} />
@@ -32,6 +38,12 @@ export function Encoding({ encoder, model, parameters }: Props) {
   }
   if (encoder === TermFrequencyEncoder) {
     return <TermFrequencyEncoding model={model} parameters={parameters} />
+  }
+  if (encoder === EmbeddingsEncoder) {
+    return <EmbeddingsEncoding model={model} parameters={parameters} />
+  }
+  if (encoder === TriplesEncoder) {
+    return <TriplesEncoding model={model} parameters={parameters} />
   }
   return <EncodingFallback encoder={encoder} model={model} parameters={parameters} />
 }
--- packages/visualizer/src/lib/useModelState.ts
+++ packages/visualizer/src/lib/useModelState.ts
@@ -63,10 +63,10 @@ export const useModelState = createSelectors(
           } = get()
           const newParameters = parser
             ? getNewParameters(
-              parser.parameters,
-              oldParameters,
-              oldParser?.parameters,
-            )
+                parser.parameters,
+                oldParameters,
+                oldParser?.parameters,
+              )
             : oldParameters
           const { model, error } = tryParse(
             parser,
